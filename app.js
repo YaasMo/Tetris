e1841247182ad9384d07492970c493b2c9a1e79f
Yaaseen Mahomed <yaaseen@Yaaseens-MacBook-Pro.local>
2020-05-22 00:06:36 +0200

    storing grid divs in 2d array

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => { //in order for code to be picked up by html file
-
+    const grid = document.querySelector('.grid')
+    // let squares = Array.from(document.querySelectorAll('.grid div'))
+    let squares = document.querySelectorAll('.grid div')
+    let squareArray = Array.from(squares)
     
+    const width = 10
+
+
 
 })
